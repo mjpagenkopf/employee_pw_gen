7c7b1557327ca0ce4e1a2de1afcd9ceb93bec744
mjpagenkopf <78058404+mjpagenkopf@users.noreply.github.com>
2021-03-07 11:18:40 -0500

    made more modifications to the js

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,49 @@
 // Assignment Code
 let generateBtn = document.querySelector("#generate"); //THIS IS THE BUTTON
 // console.log(generateBtn.parentElement); - showed the parent element for the id generate
-let uppercaseRun = document.querySelector("#uppercase");
-let lowercaseRun = document.querySelector("#lowercase");
+let results = document.querySelector("card-header")
+let upperRun = document.querySelector("#uppercase");
+let lowerRun = document.querySelector("#lowercase");
 let numberRun = document.querySelector("#number");
 let specialRun = document.querySelector("#special");
+let lengthRun = document.querySelector("#length");
 
-let length = "";
-let numbers = "0123456789";
-let uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";  //["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-let lowercase = "abcdefghijklmnopqrstuvwxyz"; //["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-let special = "!@#$%^&*()_+{}<>?-=/"; //["!", "#", "$", "%", "&", "(", ")", "*", "+", "/", "<", "=", ">", "?", "@", "[", "]", "{", "}"];
+// let number = "0123456789";
+// let uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";  //["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+// let lowercase = "abcdefghijklmnopqrstuvwxyz"; //["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+// let special = "!@#$%^&*()_+{}<>?-=/"; //["!", "#", "$", "%", "&", "(", ")", "*", "+", "/", "<", "=", ">", "?", "@", "[", "]", "{", "}"];
 
+let randomFunction = {
+  upper: getUpper,
+  lower: getLower,
+  number: getNumber,
+  special: getSpecialCharacter,
+}
+
+// card-headers.addEventListener("click",() => {
+//   let textarea = document.querySelector("textarea");
+//   let password = results.innerText;
+
+//   if (!password) {return;}
+
+//   textarea.value = password;
+//   document.body.appendChild(textarea);
+//   textarea.select();
+//   document.execCommand("copy");
+//   textarea.remove();
+//   alert("Password copied to clipboard");
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword) {
+  let length = +lengthRun.value;
+  let checkNumber = numberRun.checked;
+  let checkUpper = upperRun.checked;
+  let checkLower = lowerRun.checked;
+  let checkSpecial = specialRun.checked;
+
+  results.innerText = generatePassword (
+    length, checkNumber, checkUpper, checkLower, checkSpecial);
+});
 
 
 // Write password to the #password input
@@ -23,40 +55,45 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-  let length = (+length.value);
 
+  function generatePassword(length, uppercase, lowercase, number, special) {
 
-function generatePassword(length, numbers, lowercase, uppercase, special) {
-  let length = prompt("Must be at least 8 but no more than 128.");
-    if (length < 8) {
-      console.log(false);
+    let randomPasswordGenerated = "";
+
+    let count = uppercase + lowercase + number + special;
+
+    let countArray = [{uppercase}, {lowercase}, {number}, {special}].filter (item => Object.values(item)[0]);
+
+    if (count === 0) {
+      return "";
     }
 
-let randomFunction = {
-  number: getNumbers,
-  uppercase: getUppercase,
-  lowercase: getLowercase,
-  specialChar: getSpecialCharacters,
-};
+    for (let i = 0; i < length; i += count) {
+      countArray.forEach(type => {
+          let functionName = Object.keys(type)[0];
+
+          generatePassword += randomFunction[functionName]();
+      })
+    
+  }
+
 
 // Generator functions**
-  function getNumbers {
+  function getNumber() {
     return String.fromCharCode(Math.floor(Math.random() * 10 + 48));
-  },
+  }
 
   function getUppercase() {
     return String.fromCharCode(Math.floor(Math.random() * 26 + 97));
-  },
+  }
 
   function getLowercase() {
     return String.fromCharCode(Math.floor(Math.random() * 26 + 65));
-  },
+  }
 
-  function getSpecialCharacters() {
-    let specialCharacters = "!@#$%^&*()_+-={}[]<>/?";
-    return specialCharacters[Math.floor(Math.random() * specialCharacters.length)];
+  function getSpecialCharacter() {
+    let specialCharacter = "!@#$%^&*()_+-={}[]<>/?";
+    return specialCharacter[Math.floor(Math.random() * specialCharacter.length)];
   }
 
 
@@ -65,7 +102,7 @@ let randomFunction = {
 
 // if (length === )
 
-if (numbers === true) {
+if (number === true) {
   minimumNumbers = functionArray.getNumbers();
   minimumCount++;
 
@@ -88,26 +125,20 @@ if (special === true) {
   minimumCount++;
 
 }
+};
 
-// empty string variable for the for loop below
-var randomPasswordGenerated = "";
 
-// loop getting random characters
-for (let i = 0; i < (parseInt(passwordLength) - minimumCount); i++) {
-  var randomNumberPicked = Math.floor(Math.random() * 4);
+// // loop getting random characters
+// for (let i = 0; i < (parseInt(passwordLength) - minimumCount); i++) {
+//   var randomNumberPicked = Math.floor(Math.random() * 4);
 
-  randomPasswordGenerated += randomNumberPicked;
+//   randomPasswordGenerated += randomNumberPicked;
 
-}
+// }
 
 // to make sure characters are added to the password
-randomPasswordGenerated += minimumNumbers;
-randomPasswordGenerated += minimumLowerCases;
-randomPasswordGenerated += minimumUpperCases;
-randomPasswordGenerated += minimumSpecialCharacters;
-
-
-return randomPasswordGenerated;
-
-}
+// randomPasswordGenerated += minimumNumbers;
+// randomPasswordGenerated += minimumLowerCases;
+// randomPasswordGenerated += minimumUpperCases;
+// randomPasswordGenerated += minimumSpecialCharacters;
 
